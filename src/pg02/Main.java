83014ad415e087c465641232eab46e877bf57b8e
victorcas04 <victorcas95@gmail.com>
2018-03-07 09:06:55 +0100

    Initial commit

---
+++ src/pg02/Main.java
@@ -0,0 +1,22 @@
+package pg02;
+
+/**
+ * 
+ * @author victor
+ *
+ */
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Ball ball = new Ball();
+		//Board board = new Board();
+		Billiards billiards = new Billiards();
+	}
+
+}
